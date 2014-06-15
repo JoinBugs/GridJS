0d6f819968f6ccc2ddeb75d9e397d41e584e1fba
Andres <andres_pineapple@hotmail.com>
2014-06-15 08:57:09 -0500

    it's full the first demo, where do you see a table of users created with gridjs

--- demo/js/index.js
+++ demo/js/index.js
@@ -4,7 +4,25 @@
 
 	function init()
 	{
-		
+		var users = [
+			{ 'id' : 0, 'name' : 'Andres', 'isMarried' : false, 'pets' : [ 'Dog', 'Turtle'] },
+			{ 'id' : 1, 'name' : 'Jorge', 'isMarried' : true, 'pets' : [ 'Bird', 'Fish', 'Dog'] },
+			{ 'id' : 2, 'name' : 'Lucy', 'isMarried' : false, 'pets' : [ 'Duck', 'Eagle', 'Delphi' ] },
+			{ 'id' : 3, 'name' : 'Yechis', 'isMarried' : true, 'pets' : [ 'Cuyo', 'Hasmter', 'Turle'] },
+			{ 'id' : 4, 'name' : 'Maria', 'isMarried' : false, 'pets' : [ 'Bird', 'Dog'] },
+			{ 'id' : 5, 'name' : 'Pedro', 'isMarried' : true, 'pets' : [ 'Horse', 'Ponny', 'Cown' ] },
+	    ],
+
+			table = grid.createGrid(
+		    {
+		        'data': users,
+		        'appendNode': 'body',
+		        'responsive' : 'Usuario',
+		        'attrs': {
+		             'id': 'tblKardex',
+		             'class': 'table'
+		        }
+		    });
 	}
 })
 ( window, document );
